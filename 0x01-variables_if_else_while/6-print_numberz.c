34899281062d596cd0d71750587742dda83b9393
Chrisblurp <adeyemi504@gmail.com>
2022-03-18 08:07:19 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* Main - Entry Point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+int digit;
+for (digit = 40; digit <= 57; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
