19e05f99fab7b8aa97ac291acff29971c335621e
Chrisblurp <adeyemi504@gmail.com>
2022-03-19 18:13:57 -0700

    printing the single digit numbers with comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * Main - Entry point
+  *
+  * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+if (num == 57)
+{break;
+}
+putchar('.');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
