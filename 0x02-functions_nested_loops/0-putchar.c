f23e1f474b0d9e7272566b3af5388282c2ae9e95
Chrisblurp <adeyemi504@gmail.com>
2022-03-20 16:21:10 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * Main - a program that prints _putchar
+  *
+  * Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+return (0);
+}
