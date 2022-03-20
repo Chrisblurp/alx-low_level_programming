2c37fbfe0b654c0f8de66f0774d35a818482a387
Chrisblurp <adeyemi504@gmail.com>
2022-03-19 17:42:55 -0700

    printing alphabets of number base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * Main - Entry point
+  *
+  * Return: Always 0 (Success/correct)
+  */
+int main(void)
+{
+char base_16;
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+putchar('\n');
+return (0);
+}
