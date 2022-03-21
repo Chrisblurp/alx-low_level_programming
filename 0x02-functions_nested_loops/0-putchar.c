cbbcc893ac450081f43b97f03d47525d98353458
Chrisblurp <adeyemi504@gmail.com>
2022-03-21 07:29:45 -0700

    printing alphabets x10

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-  * Main - a program that prints _putchar
-  *
-  * Return: nothing return void
+* Main - a program that prints _putchar
+*
+* Return: nothing return void
 */
 int main(void)
 {
 _putchar('_');
-_putchar('p');		          
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
 _putchar('h');
-_putchar('a'); 
+_putchar('a');
 _putchar('r');
 _putchar ('\n');
 return (0);
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-   * print_alphabet - print alphabet lowercase
-   *
-   * Return: Always 0
- */
+* print_alphabet - print alphabet lowercase
+*
+* Return: Always 0
+*/
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
+
 char alpha;
-for (alpha = 'a';  alpha >= 'z'; alpha++);
+
+for (alpha = 'a';  alpha <= 'z'; alpha++);
+
 _putchar(alpha);
+
 _putchar('\n');
+
 }
