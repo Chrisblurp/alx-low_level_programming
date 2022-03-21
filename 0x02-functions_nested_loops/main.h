c18aa7df3f81df7c73815178641b127fd429cce2
Chrisblurp <adeyemi504@gmail.com>
2022-03-21 05:49:13 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,11 @@
+#include "0-putchar.c"
+#include <unistd.h>
+/**
+   * _putchar - writes the character c to stdout
+   *
+   * Return: nothing (void)
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
