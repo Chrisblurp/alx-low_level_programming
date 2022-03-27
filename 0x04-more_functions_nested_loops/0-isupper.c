205900d2248a2b4579d5382092fffe19c323b032
Chrisblurp <adeyemi504@gmail.com>
2022-03-26 18:41:35 -0700

    checking if a character iosa digit wiith isdigit functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _isuper function that checks for uppercase character
- * @c: contains value to be compared
- * Return: Always 0
- */
+* _isuper - checks for uppercase character
+* @c: contains value to be compared
+* Return: Always 0
+*/
 int _isupper(int c)
 {
+
 if (c >= 'A' && c <= 'Z')
 {
 return (1);
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isdigit - evalue c is a digit
+* @c: checks for the value of c
+* Return: 0 Always.
+*/
+int _isdigit(int c)
+{
+
+if (c = '0' && c < '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
+
+
